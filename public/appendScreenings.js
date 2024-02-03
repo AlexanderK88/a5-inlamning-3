b50007aaa2d13a5a813495df0e77d77952474cc9
EmilioSanchezLernia <emilio.sanchez@lerniastudent.se>
2024-02-03 19:17:49 +0100

    Added logic for fetching and appening screening times

---
+++ public/appendScreenings.js
@@ -0,0 +1,57 @@
+async function fetchScreenings() {
+  const response = await fetch("/api/recent-screenings");
+  const originalData = await response.json();
+  const relevantData = originalData.map((screening) => ({
+    start_time: convertToEuropeanTime(screening.attributes.start_time),
+    start_date: convertToEuropeanDate(screening.attributes.start_time),
+    room: screening.attributes.room,
+    title: screening.attributes.movie.data.attributes.title,
+  }));
+
+  for (let i = 0; i < relevantData.length; i++) {
+    const screeningData = appendScreenings(relevantData[i]);
+    document.querySelector(".frontpage__screenings").appendChild(screeningData);
+  }
+}
+
+fetchScreenings();
+
+function appendScreenings(screeningData) {
+  const screeningContainer = document.createElement("div");
+  screeningContainer.classList.add("frontpage__container");
+
+  const screeningTitle = document.createElement("h4");
+  screeningTitle.textContent = screeningData.title;
+  screeningTitle.classList.add("frontpage__title");
+  screeningContainer.appendChild(screeningTitle);
+
+  const screeningRoom = document.createElement("p");
+  screeningRoom.textContent = `Salong: ${screeningData.room}`;
+  screeningRoom.classList.add("frontpage__room");
+  screeningContainer.appendChild(screeningRoom);
+
+  const screeningTime = document.createElement("p");
+  screeningTime.textContent = `Start tid: ${screeningData.start_time}, ${screeningData.start_date}`;
+  screeningTime.classList.add("frontpage__time");
+  screeningContainer.appendChild(screeningTime);
+
+  return screeningContainer;
+}
+
+function convertToEuropeanTime(isoTime) {
+  const date = new Date(isoTime);
+  return date.toLocaleString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+}
+
+function convertToEuropeanDate(isoTime) {
+  const date = new Date(isoTime);
+  return date.toLocaleString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+}
