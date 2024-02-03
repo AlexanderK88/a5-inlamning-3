2f6e1f1fc1fa9ba72fc86e1e6828ff57fcf2b0ae
EmilioSanchezLernia <emilio.sanchez@lerniastudent.se>
2024-02-03 19:15:43 +0100

    Updated faulty logic

--- src/getRecentScreenings.js
+++ src/getRecentScreenings.js
@@ -1,13 +1,21 @@
 export default async function getRecentScreenings(cmsScreenings) {
   const screenings = await cmsScreenings.loadAllScreenings();
 
+  //Provides the current date as a base
+  const currentDate = new Date();
+
   const fiveDaysFromNow = new Date();
   fiveDaysFromNow.setDate(fiveDaysFromNow.getDate() + 5);
 
   return screenings
     .filter((screening) => {
       const startDate = new Date(screening.attributes.start_time);
-      return startDate < fiveDaysFromNow;
+      // Make sure the screening times is after the current date
+      const afterCurrentDate = startDate >= currentDate;
+      // Make sure the screening times are not more then 5 days in the future
+      const beforeFiveDays = startDate <= fiveDaysFromNow;
+
+      return afterCurrentDate && beforeFiveDays;
     })
     .slice(0, 10);
 }
