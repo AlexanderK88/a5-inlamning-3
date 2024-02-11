de4e1e12c68c7180bb1460f65bfb94ae6b9c8f64
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2024-02-11 14:06:12 +0100

    Added try, catch to averageRating-function

--- src/movies.js
+++ src/movies.js
@@ -48,44 +48,48 @@ export async function getMovieScreenings(cmsAdapter, id) {
   }
 }
 export async function averageRating(id) {
-  const response = await fetch(
-    API_BASE + "/reviews?populate=movie&filters[movie]=" + id
-  );
-  const payload = await response.json();
-  const newArray = payload.data.map((object) => {
-    return {
-      rating: object.attributes.rating,
-      imdbId: object.attributes.movie.data.attributes.imdbId,
-    };
-  });
-  const averageRatingValue = newArray.map((item) => {
-    const ratingValues = item.rating;
-    return ratingValues;
-  });
-  const changeList = averageRatingValue.filter((object) => object !== null);
-
-  async function calculateAverage(array) {
-    let sum = 0;
-    let arraylength = array.length;
-
-    array.forEach(async (obj) => {
-      sum += obj;
-      return sum;
+  try {
+    const response = await fetch(
+      API_BASE + "/reviews?populate=movie&filters[movie]=" + id
+    );
+    const payload = await response.json();
+    const newArray = payload.data.map((object) => {
+      return {
+        rating: object.attributes.rating,
+        imdbId: object.attributes.movie.data.attributes.imdbId,
+      };
     });
-    const test = sum / arraylength;
-    return test;
-  }
-  const averageValue = await calculateAverage(changeList);
+    const averageRatingValue = newArray.map((item) => {
+      const ratingValues = item.rating;
+      return ratingValues;
+    });
+    const changeList = averageRatingValue.filter((object) => object !== null);
 
-  const imdbMovieID = newArray[0].imdbId;
-  const OMDB_rating = await fetch(
-    "https://www.omdbapi.com/?apikey=bf65b015&i=" + imdbMovieID
-  );
-  const OMDB_payload = await OMDB_rating.json();
+    async function calculateAverage(array) {
+      let sum = 0;
+      let arraylength = array.length;
 
-  if (newArray.length < 5) {
-    return OMDB_payload.imdbRating;
-  } else {
-    return averageValue.toFixed(1);
+      array.forEach(async (obj) => {
+        sum += obj;
+        return sum;
+      });
+      const test = sum / arraylength;
+      return test;
+    }
+    const averageValue = await calculateAverage(changeList);
+
+    const imdbMovieID = newArray[0].imdbId;
+    const OMDB_rating = await fetch(
+      "https://www.omdbapi.com/?apikey=bf65b015&i=" + imdbMovieID
+    );
+    const OMDB_payload = await OMDB_rating.json();
+
+    if (newArray.length < 5) {
+      return OMDB_payload.imdbRating;
+    } else {
+      return averageValue.toFixed(1);
+    }
+  } catch (error) {
+    console.log(error.message);
   }
 }
