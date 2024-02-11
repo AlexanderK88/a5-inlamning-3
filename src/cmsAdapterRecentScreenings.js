a2d8167b53cfe7e78e2a54ab3e4a4f3a5ca3a6ba
EmilioSanchezLernia <emilio.sanchez@lerniastudent.se>
2024-02-11 16:24:25 +0100

    Fixed pagnation issue

--- src/cmsAdapterRecentScreenings.js
+++ src/cmsAdapterRecentScreenings.js
@@ -1,9 +1,18 @@
 const cmsAdapterRecentScreenings = {
   async loadAllScreenings() {
-    const response = await fetch(
+    const initialResponse = await fetch(
       "https://plankton-app-xhkom.ondigitalocean.app/api/screenings?populate=movie",
     );
-    const payload = await response.json();
+
+    const initialData = await initialResponse.json();
+    const totalItems = initialData.meta.pagination.total;
+    console.log(totalItems);
+
+    const pagnatedResponse = await fetch(
+      `https://plankton-app-xhkom.ondigitalocean.app/api/screenings?populate=movie&pagination[pageSize]=${totalItems}`,
+    );
+
+    const payload = await pagnatedResponse.json();
     return payload.data;
   },
 };
