4fac18ae56d46a298b788746a6f4304d0c822648
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2024-03-09 13:15:47 +0100

    Added logic so that average rating-value is displayed for every movie.

--- src/movies.js
+++ src/movies.js
@@ -84,7 +84,7 @@ export async function averageRating(id) {
     );
     const OMDB_payload = await OMDB_rating.json();
 
-    if (newArray.length < 5) {
+    if (newArray.length < 15) {
       return OMDB_payload.imdbRating;
     } else {
       return averageValue.toFixed(1);
